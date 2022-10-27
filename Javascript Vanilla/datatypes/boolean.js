96383db7ed8dcff3c6f442de990b2e34e99501fc
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2022-10-27 22:07:33 +0100

    Create boolean.js

---
+++ Javascript Vanilla/datatypes/boolean.js
@@ -0,0 +1,10 @@
+// Boolean
+
+const variableTrue = true;
+const variableFalse = false;
+
+// True is also equal to 1
+console.log(variableTrue + 0);
+
+// false is also equal to 0;
+console.log(variableFalse + 0);
