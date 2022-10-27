4e30ace60fc1727490a352b9558864545ec866fa
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2022-10-27 22:07:08 +0100

    Create cwd.js

---
+++ Frameworks/node.js/global objects/process/cwd.js
@@ -0,0 +1,7 @@
+// process.cwd
+// "cwd" = current working directory
+//The process.cwd() method returns the current working directory of the Node.js process.
+
+import { cwd } from "process";
+
+console.log(`Current directory: ${cwd()}`);
