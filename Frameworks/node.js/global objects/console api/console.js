ad230838efb244d9f815cc50bc853fc1a20e89ea
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2022-10-27 22:07:02 +0100

    Create console.js

---
+++ Frameworks/node.js/global objects/console api/console.js
@@ -0,0 +1,26 @@
+// console.log() : black colour text with no icon
+console.log();
+
+// console.count()
+console.count("string");
+// returns the number of times the string has been run in the console.
+
+// console.clear() : wipes the console log.
+
+// console.warn("warn") : Yellow color text with icon (in chrome browser)
+console.warn("warn");
+
+// console.info("info") : blue color text with icon (in chrome browser)
+console.info("info");
+
+// console.info("error") : red color text with icon (in chrome browser)
+console.info("error");
+
+// console.table() : prints the content in a table format in the console.
+console.table([
+  { a: 1, b: 2 },
+  { a: 5, b: 5 },
+]);
+
+// console.debug() :
+console.debug();
