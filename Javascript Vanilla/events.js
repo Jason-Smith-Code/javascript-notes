b9186831bceefd4ef878428243717441ea4241fa
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2022-10-27 22:07:51 +0100

    Create events.js

---
+++ Javascript Vanilla/events.js
@@ -0,0 +1,12 @@
+// Events are the triggers
+
+// an event attribute can be applied to an element in html
+// eventName={}
+
+// onclick={}  when the html element is clicked
+// onchange={}  when the html element has changed
+// onmouseover={}   when the user hovers over the html element
+// onmouseout={}    when the user has hovered over the html element, but then leaves the hover area
+// onkeydown={} when a key is pressed
+// onload={}    when the browser has finished loading the page
+// https://www.w3schools.com/jsref/dom_obj_event.asp
