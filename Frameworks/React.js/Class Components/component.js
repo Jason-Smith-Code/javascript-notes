e5b552a97b503c87c679637c059f081f3ddb7114
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2023-01-19 15:59:16 +0000

    Create component.js

---
+++ Frameworks/React.js/Class Components/component.js
@@ -0,0 +1,8 @@
+// React : Class commponents
+
+class Car extends React.Component {
+    render() {
+        return <h2>Hi, I am a Car!</h2>;
+    }
+}
+// this is a component, and can be used inside other components by inserting it as <Car />
