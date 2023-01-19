57fefc40cd4091296ad0994259f996f671d22441
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2023-01-19 16:00:01 +0000

    Update keywords.ts

--- Typescript/keywords.ts
+++ Typescript/keywords.ts
@@ -1,2 +1,4 @@
 // readonly : can prevent arrays from being changed.
 const names: readonly string[] = ["Dylan"];
+
+// keyOf : a keyword in TypeScript which is used to extract the key type from an object type.
