fda401b9ecc5f6e6dd3d39778f2e96fa35c5e074
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2023-01-19 15:59:50 +0000

    Update enumeration.ts

--- Typescript/enumeration.ts
+++ Typescript/enumeration.ts
@@ -4,8 +4,24 @@
 enum ProductType {
     Tomatos, // default value 0
     Cucumber, // 1
-    Chocolate  // 2
- }
+    Chocolate, // 2
+}
 
- // accessing an enum value
- let foodType = ProductType.Chocolate;
+// giving enum number values
+enum StatusCodes {
+    NotFound = 404,
+    Success = 200,
+    Accepted = 202,
+    BadRequest = 400,
+}
+
+// giving enum string values
+enum CardinalDirections {
+    North = "North",
+    East = "East",
+    South = "South",
+    West = "West",
+}
+
+// accessing an enum value
+let foodType = ProductType.Chocolate;
