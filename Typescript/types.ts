d8c5118d086afff9f145f05d1278605cb6c15d61
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2023-01-19 16:00:08 +0000

    Update types.ts

--- Typescript/types.ts
+++ Typescript/types.ts
@@ -1,14 +1,18 @@
 // typescript Types
 
+// static types ??
+
+// asyncronous coding in Typescript
+
 // boolean
 // string
 // number
 // array
-// undefined
-// null
+// undefined : By undefined handling is disabled, and can be enabled by setting strictNullChecks to true.
+// null : By default null handling is disabled, and can be enabled by setting strictNullChecks to true.
 // void : The type void can be used to indicate a function doesn't return any value
 function printHello(): void {
-    console.log('Hello!');
+    console.log("Hello!");
 }
 // any
 // unknown
@@ -29,32 +33,45 @@ let implicitAssigned = "Dylan";
 // create a new type
 
 type TypeNames = {
-    id: number,
-    name: string,
-    icon?: string 
-}
+    id: number;
+    name: string;
+    icon?: string;
+};
 
 // "TypeNames" can now be used as a type when checking against variables
 let variable: TypeNames["name"]; // string
 
 // Type Alias : allows you to change the nname of a type to something more suitable
-type BusYear = number
-type BusType = string
-type BusModel = string
+type BusYear = number;
+type BusType = string;
+type BusModel = string;
 type Bus = {
-  year: BusYear,
-  type: BusType,
-  model: BusModel
-}
+    year: BusYear;
+    type: BusType;
+    model: BusModel;
+};
 
-const busYear: BusYear = 2001
-const busType: BusType = "Toyota"
-const busModel: BusModel = "Corolla"
+const busYear: BusYear = 2001;
+const busType: BusType = "Toyota";
+const busModel: BusModel = "Corolla";
 const bus: Bus = {
-  year: busYear,
-  type: busType,
-  model: busModel
+    year: busYear,
+    type: busType,
+    model: busModel,
 };
 
 // union types : allows us to assign more than one data type to a variable
 let unionType: string | number | undefined;
+
+// Type casting : Overriding a type
+// Casting using "as"
+let unknown: unknown = "hello";
+console.log((unknown as string).length); // unknown as string
+
+// Casting using "<>"
+let unknown2: unknown = "hello";
+console.log((<string>unknown2).length); // <string>unknown
+
+// force casting
+let forceCast = "hello";
+// console.log((forceCast as unknown as number).length); // x is not actually a number so this will return undefined
